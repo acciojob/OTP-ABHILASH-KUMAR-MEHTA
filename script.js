d26b67551826154df371c8153985b97c0e168fc6
Abhilash Kumar Mehta <145607994+ABHILASH-KUMAR-MEHTA@users.noreply.github.com>
2025-05-04 09:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+  input.addEventListener('keydown', (e) => {
+    const key = e.key;
+
+    if (key >= 0 && key <= 9) {
+      input.value = '';
+      setTimeout(() => {
+        if (index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      }, 10);
+    } else if (key === 'Backspace') {
+      input.value = '';
+      setTimeout(() => {
+        if (index > 0) {
+          inputs[index - 1].focus();
+        }
+      }, 10);
+    } else {
+      e.preventDefault(); // prevent non-numeric input
+    }
+  });
+});
